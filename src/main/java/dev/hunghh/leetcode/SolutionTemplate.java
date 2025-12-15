89764d89540f0d1d75cd4265773873224eba08fb
hunghh.dev <hunghh.dev@gmail.com>
2025-12-15 20:56:49 +0700

    Initial commit: Maven project for LeetCode practice

---
+++ src/main/java/dev/hunghh/leetcode/SolutionTemplate.java
@@ -0,0 +1,15 @@
+package dev.hunghh.leetcode;
+
+/**
+ * LeetCode #XXX - Problem Name
+ * https://leetcode.com/problems/problem-name/
+ *
+ * Description:
+ * TODO: Add problem description here
+ */
+public class SolutionTemplate {
+
+    public void solve() {
+        // TODO: Implement solution
+    }
+}
---
+++ src/test/java/dev/hunghh/leetcode/SolutionTemplateTest.java
@@ -0,0 +1,43 @@
+package dev.hunghh.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for LeetCode #XXX - Problem Name
+ */
+class SolutionTemplateTest {
+
+    private SolutionTemplate solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new SolutionTemplate();
+    }
+
+    @Test
+    @DisplayName("Example 1")
+    void testExample1() {
+        // TODO: Add test case
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    @DisplayName("Example 2")
+    void testExample2() {
+        // TODO: Add test case
+    }
+
+    @Test
+    @DisplayName("Edge case")
+    void testEdgeCase() {
+        // TODO: Add edge case test
+    }
+}
