1a5d82e2edc70e604b7a6ea22e11164720ff2403
hunghh.dev <hunghh.dev@gmail.com>
2025-12-17 23:27:03 +0700

    feat(p00026): solve Remove Duplicates from Sorted Array

--- src/main/java/dev/hunghh/leetcode/p00026/Solution.java
+++ src/main/java/dev/hunghh/leetcode/p00026/Solution.java
@@ -19,6 +19,13 @@ package dev.hunghh.leetcode.p00026;
 public class Solution {
 
     public int removeDuplicates(int[] nums) {
-
+        int len = nums.length;
+        int j = 0;
+        for (int i = 1; i < len; i++) {
+            if (nums[j] != nums[i]) {
+                nums[++j] = nums[i];
+            }
+        }
+        return j + 1;
     }
 }
